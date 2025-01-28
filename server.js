8b086a8a30e397dc8481efc8084fcf573d403cfb
MistaCareca <brennno0cartaxo@gmail.com>
2025-01-28 01:28:32 -0300

    mais modificaçoes de back

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const encurtarUrls = require('./models/encurtaUrls');
+
+mongoose.connect("mongodb://localhost/encurtarUrls");
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) =>{
-    res.render('index');
-})
+app.get('/', async (req, res) => {
+    const urls = await encurtarUrls.find();
+    res.render('index', { encurtarUrls: urls });
+});
+
+app.post('/encurtarUrls', async (req, res) => {
+    await encurtarUrls.create({ full: req.body.fullUrl });
+    res.redirect('/');
+});
 
 app.listen(process.env.PORT || 3000);
